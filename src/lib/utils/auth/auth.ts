eb14ed60d812ed7a20230176f3ddc713a14c7e37
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2026-04-14 23:27:29 +0700

    dont hardcode better auth secret

--- src/lib/utils/auth/auth.ts
+++ src/lib/utils/auth/auth.ts
@@ -30,6 +30,7 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
+  secret: process.env.BETTER_AUTH_SECRET,
   emailAndPassword: {
     enabled: true,
     requireEmailVerification: true,
