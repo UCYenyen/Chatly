0b5a7c22d5874b357be9d8845ff6b78b10264e4f
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2026-04-15 09:23:22 +0700

    add typing effect

--- src/components/features/landing/HeroSection.tsx
+++ src/components/features/landing/HeroSection.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import { useRef } from "react";
 import CountUp from "../../personal/CountUp";
 import { useGsapScrollReveal } from "@/hooks/use-gsap-scroll-reveal";
-
+import TextType from "@/components/TextType";
 export function HeroSection() {
   const sectionRef = useRef<HTMLElement | null>(null);
   useGsapScrollReveal(sectionRef, { start: "top 86%", y: 36, fade: false });
@@ -19,13 +19,30 @@ export function HeroSection() {
         <div className="flex items-center gap-2 bg-surface-container-high border border-outline-variant/15 w-fit rounded-full px-3 py-1 mb-2">
           <div className="w-1.5 h-1.5 rounded-full bg-secondary-fixed shadow-[0_0_5px_rgba(164,215,48,0.8)]"></div>
           <span className="text-[10px] font-mono text-outline uppercase tracking-widest font-bold">
-            Sistem Online
+            <TextType
+              text={["System Online", "Hi, I am chatly"]}
+              typingSpeed={75}
+              pauseDuration={1500}
+              showCursor
+              cursorCharacter="_"
+              deletingSpeed={50}
+              cursorBlinkDuration={0.5}
+            />
           </span>
         </div>
 
         <h1 className="text-5xl lg:text-6xl xl:text-7xl font-headline font-bold text-on-surface leading-[1.1] tracking-tight">
           Your Business, <br />
-          responding <span className="text-secondary-fixed">24/7.</span>
+
+          <TextType
+              text={["Responding", "Always on"]}
+              typingSpeed={75}
+              pauseDuration={3500}
+              showCursor
+              cursorCharacter=""
+              deletingSpeed={100}
+              cursorBlinkDuration={0.5}
+            /> <span className="text-secondary-fixed">24/7.</span>
         </h1>
 
         <p className="text-[16px] xl:text-[18px] text-outline leading-relaxed max-w-lg mt-2 mb-4">
@@ -38,7 +55,7 @@ export function HeroSection() {
           <Button className="bg-[#bff44c] text-[#141f00] hover:bg-[#a4d730] font-bold text-[13px] h-12 px-8 rounded-sm shadow-md transition-transform active:scale-95 border border-[#a4d730]">
             Luncurkan Agen Anda
           </Button>
-          <Button className="bg-transparent text-outline hover:text-on-surface hover:bg-surface-container font-medium text-[13px] h-12 px-8 rounded-sm border border-outline-variant/20 transition-all active:scale-95">
+          <Button className="bg-transparent text-outline hover:text-on-surface hover:bg-surface-container font-medium text-[13px] h-12 px-8 rounded-sm border border-outline-variant transition-all active:scale-95">
             Lihat Dokumentasi
           </Button>
         </div>
