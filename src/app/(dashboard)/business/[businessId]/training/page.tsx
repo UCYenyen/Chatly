d494a75dfa0b8683fa8368298e0c63688c46ba87
A-Eugene <amadeuseugenedirgantara@gmail.com>
2026-05-05 15:06:15 +0700

    Disable training preview

--- src/app/(dashboard)/business/[businessId]/training/page.tsx
+++ src/app/(dashboard)/business/[businessId]/training/page.tsx
@@ -7,29 +7,29 @@ import { TrainingFooter } from "@/components/features/training/TrainingFooter";
 import { ActiveBusinessBanner } from "@/components/features/business/ActiveBusinessBanner";
 
 export default function TrainingPage() {
-  return (
-    <div className="p-8 lg:p-12 xl:p-14 w-full mx-auto max-w-[1500px] flex flex-col min-h-full">
-      <TrainingHeader />
+    return (
+        <div className="p-8 lg:p-12 xl:p-14 w-full mx-auto max-w-[1500px] flex flex-col min-h-full">
+            <TrainingHeader />
 
-      <div className="mt-6">
-        <ActiveBusinessBanner scopeLabel="Pelatihan AI" />
-      </div>
+            <div className="mt-6">
+                <ActiveBusinessBanner scopeLabel="Pelatihan AI" />
+            </div>
 
-      <div className="grid grid-cols-1 xl:grid-cols-12 gap-8 my-8 flex-1">
-        <div className="col-span-1 xl:col-span-7 flex flex-col">
-          <KnowledgeBase />
-          <ExternalData />
+            <div className="grid grid-cols-1 xl:grid-cols-12 gap-8 my-8 flex-1">
+                <div className="col-span-1 xl:col-span-7 flex flex-col">
+                    <KnowledgeBase />
+                    <ExternalData />
+                </div>
+
+                <div className="col-span-1 xl:col-span-5 flex flex-col">
+                    <AiPersonality />
+                    {/* <TrainingPreview /> */}
+                </div>
+            </div>
+
+            <div className="mt-8">
+                <TrainingFooter />
+            </div>
         </div>
-
-        <div className="col-span-1 xl:col-span-5 flex flex-col">
-          <AiPersonality />
-          <TrainingPreview />
-        </div>
-      </div>
-
-      <div className="mt-8">
-        <TrainingFooter />
-      </div>
-    </div>
-  );
+    );
 }
